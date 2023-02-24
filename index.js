29b1465ac79210144d4fedd091f86b06ee040822
kamil-alperen <kamilalperenyilmaz11235@gmail.com>
2023-02-24 21:09:50 +0300

    build changed 2

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Add Express
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
 
-/* const deviceIdList = []; */
+const deviceIdList = [];
 
 // Initialize Express
 const app = express();
@@ -19,20 +18,11 @@ app.post("/authenticate", (req, res) => {
 
   let response = "NO";
 
-  const deviceIDs = fs.readFileSync("deviceID.txt", {encoding:'utf8', flag:'r'});
-  deviceIDs.split("\n")?.forEach(deviceID => {
-    if (deviceID === unity_deviceId) {
-      response = "OK";
-    }
-  })
-
   res.send(response);
 })
 
 app.post("/login_success", (req, res) => {
   let web_deviceId = req.body.deviceId;
-  fs.appendFileSync('deviceID.txt', web_deviceId + "\n");
-  res.send("PUSHED");
 })
 
 // Initialize server
